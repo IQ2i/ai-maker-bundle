8443c03eff4cc56a941406610f6701fa7db89b86
Loïc Sapone <loic@mezcalito.fr>
2025-04-26 17:46:37 +0200

    Init commit

---
+++ rector.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the AI Maker Bundle.
+ *
+ * (c) Loïc Sapone <loic@sapone.fr>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+use Rector\CodeQuality\Rector\Identical\FlipTypeControlToUseExclusiveTypeRector;
+use Rector\Config\RectorConfig;
+use Rector\PHPUnit\Set\PHPUnitSetList;
+use Rector\Set\ValueObject\LevelSetList;
+use Rector\Set\ValueObject\SetList;
+
+return static function (RectorConfig $rectorConfig): void {
+    $rectorConfig->paths([
+        __DIR__.'/src',
+        __DIR__.'/tests',
+    ]);
+
+    $rectorConfig->sets([
+        SetList::CODE_QUALITY,
+        SetList::CODING_STYLE,
+        LevelSetList::UP_TO_PHP_83,
+        PHPUnitSetList::PHPUNIT_110,
+    ]);
+
+    $rectorConfig->skip([
+        __DIR__.'/tests/fixtures/*',
+        FlipTypeControlToUseExclusiveTypeRector::class,
+    ]);
+};
